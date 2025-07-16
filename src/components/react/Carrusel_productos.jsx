c44768264f6dbd3f151eb371568347d5918ec9fd
jeison perdomo <jeisonperdomo@online.com>
2025-07-15 20:30:07 -0400

    Ya esta el carrusel de los productos solo falta añadir las cards

--- src/components/react/Carrusel_productos.jsx
+++ src/components/react/Carrusel_productos.jsx
@@ -1,20 +1,28 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
 import 'swiper/css';
+import 'swiper/css/navigation';
 
 const Carrusel_productos = () => {
   return (
-    <div className="swiper h-100">
-  <div className="swiper-wrapper">
-    <div className="swiper-slide"> <img alt="imagen 1" /> </div>
-    <div className="swiper-slide"> <img alt="imagen 2" /> </div>
-    <div className="swiper-slide"> <img alt="imagen 3" /> </div>
-  </div>
-  <div className="swiper-pagination" />
-  <div className="swiper-button-prev" />
-  <div className="swiper-button-next" />
-</div>
+    <Swiper
+      spaceBetween={50}
+      slidesPerView={4}
+      navigation={true}         
+      modules={[Navigation]}      
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide>Producto 1</SwiperSlide>
+      <SwiperSlide>Producto 2</SwiperSlide>
+      <SwiperSlide>Producto 3</SwiperSlide>
+      <SwiperSlide>Producto 4</SwiperSlide>
+      <SwiperSlide>Producto 5</SwiperSlide>
+      <SwiperSlide>Producto 6</SwiperSlide>
+      <SwiperSlide>Producto 7</SwiperSlide>
+      <SwiperSlide>Producto 8</SwiperSlide>
+    </Swiper>
+  );
+};
 
-
-)}
-
-export default Carrusel_productos
+export default Carrusel_productos;
